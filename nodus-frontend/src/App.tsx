6a134ff7a376b1f1e78563f56c038e6f4f58f11c
unknown <moonlightfiko@gmail.com>
2026-04-27 22:51:22 -0700

    feat: profile page and setting

--- nodus-frontend/src/App.tsx
+++ nodus-frontend/src/App.tsx
@@ -1,9 +1,14 @@
-export default function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Profile from './pages/profile/profile.tsx';  
+
+function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black">
-      <h1 className="text-4xl font-bold text-white">
-        Tailwind is working 🚀
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
